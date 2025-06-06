ad40f3eb2b759126ab78bcad8f1997e9564d85b7
Amadeus <rogelioelizondos@gmail.com>
2025-06-06 16:28:20 -0600

    fix login, changed padding of .main content, changed font, removed home and fixed users.html .02

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-id_7q%f1rr@)q#2l@i&0i8*27t1zv52i%b70v-msc_))kokpin'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -58,7 +58,6 @@ CSRF_TRUSTED_ORIGINS = [
 ROOT_URLCONF = 'dcrm.urls'
 
 TEMPLATES = [
-
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
