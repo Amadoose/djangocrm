7ed43fb0eb9a9b9de7e26b5b14fada6bd1b18a54
Amadeus <rogelioelizondos@gmail.com>
2025-06-07 10:59:10 -0600

    glass login

--- website/views.py
+++ website/views.py
@@ -21,8 +21,7 @@ def login_user(request):
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
-            return redirect('users')  # Redirect to home after login
+            login(request, user) 
         else:
             return render(request, 'login.html', {'error': 'Invalid credentials, please try again.'})
     return render(request, 'login.html')
